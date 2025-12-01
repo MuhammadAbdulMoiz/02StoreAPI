3912225caf47b4323a89b758fcb44ffdc2c4d1cd
Muhammad Abdul Moiz <moiza354@gmail.com>
2025-12-01 13:58:59 +0500

    changed getAll method to have optional parameters + also added queryParameter implementtation for test

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,21 @@
 const CustomError = require('../errors/customErrors');
 const ProductCollection = require('../models/Product');
 
+// Gets all products with optional filtering by 'featured' query parameter and ignores other query parameters
 const getAllProducts = async (req, res) => {
-    const products = await ProductCollection.find({});
-    res.status(200).json({ message: products });
+    const { featured, company } = req.query;
+    const queryObj = {};
+    if (featured) {
+        queryObj.featured = featured === 'true' ? true : false;
+    }
+    if (company) {
+        queryObj.company = company;
+    }
+    const products = await ProductCollection.find(queryObj);
+    if (!products) {
+        throw new CustomError('No products found for the given query', 404);
+    }
+    res.status(200).json({ message: products, nbHits: products.length });
 }
 
 const featuredProducts = async (req, res) => {
@@ -19,7 +31,15 @@ const createProduct = async (req, res) => {
     res.status(201).json({ message: 'Create product' });
 }
 
-const getProduct = async (req, res, next) => {
+const queryProducts = async (req, res) => {
+    const products = await ProductCollection.find(req.query);
+    if (!products) {
+        throw new CustomError('No products found for the given query', 404);
+    }
+    res.status(200).json({ message: products, nbHits: products.length });
+}
+
+const getProduct = async (req, res) => {
     const { id } = req.params;
     const product = await ProductCollection.findById(id);
     if (!product) {
@@ -28,4 +48,4 @@ const getProduct = async (req, res, next) => {
     res.status(200).json({ message: product });
 }
 
-module.exports = {getAllProducts, featuredProducts, createProduct, getProduct};
+module.exports = {getAllProducts, queryProducts, featuredProducts, createProduct, getProduct};
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAllProducts, featuredProducts, createProduct, getProduct } = require('../controllers/products');
+const { getAllProducts, queryProducts, featuredProducts, createProduct, getProduct } = require('../controllers/products');
 
 router.route('/').get(getAllProducts).post(createProduct);
 router.route('/featured').get(featuredProducts);
 router.route('/:id').get(getProduct);
+router.route('/query').get(queryProducts);
 
 module.exports = router;
