cafef816bed3d346bd50fd6830f03409c3694e9c
Muhammad Abdul Moiz <moiza354@gmail.com>
2025-12-01 15:52:10 +0500

    implemented pagination using page, limit, skip + also implemented numeric filters

--- controllers/products.js
+++ controllers/products.js
@@ -4,7 +4,7 @@ const ProductCollection = require('../models/Product');
 // Gets all products with optional filtering by 'featured' query parameter and ignores other query parameters
 // Gets all products containing the matching charatcers
 const getAllProducts = async (req, res) => {
-    const { name, featured, company, sort } = req.query;
+    const { name, featured, company, sort, fields, numericFilters } = req.query;
     const queryObj = {};
     if (featured) {
         queryObj.featured = featured === 'true' ? true : false;
@@ -15,6 +15,24 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObj.name = { $regex: name, $options: 'i' };
     }
+    if (numericFilters){
+        const operatorMap = {
+            '>' : '$gt',
+            '>=' : '$gte',
+            '=' : '$eq',
+            '<' : '$lt',
+            '<=' : '$lte'
+        }
+        const regex = /\b(>|>=|=|<=|<)\b/g
+        let filters = numericFilters.replace(regex, (match)=>`-${operatorMap[match]}-`);
+        const options = ['price', 'rating'];
+        filters.split(',').forEach(element => {
+            const [field, operator, value] = element.split('-');
+            if (options.includes(field)){
+                queryObj[field] = { [operator]: Number(value) };
+            }
+        });
+    }
     let results = ProductCollection.find(queryObj);
     if (sort){
         const sortList = sort.split(',').join(' ');
@@ -22,6 +40,16 @@ const getAllProducts = async (req, res) => {
     }else {
         results = results.sort('createdAt');
     }
+    if (fields){
+        const fieldList = fields.split(',').join(' ');
+        results = results.select(fieldList);
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    results = results.skip(skip).limit(limit);
+
     const products = await results;
     if (!products) {
         throw new CustomError('No products found for the given query', 404);
