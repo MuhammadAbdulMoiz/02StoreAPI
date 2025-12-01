a9265e110bcbd529f1d0214f5da8c9c1ff21b6e2
Muhammad Abdul Moiz <moiza354@gmail.com>
2025-12-01 12:08:41 +0500

    Initial commit

---
+++ middleware/error-handler.js
@@ -0,0 +1,9 @@
+const { CustomError } = require('../errors/customError')
+const errorHandlerMiddleware = async (err, req, res, next) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).json({ msg: err.message })
+  }
+  return res.status(500).json({ msg: 'Something went wrong, please try again' })
+}
+
+module.exports = errorHandlerMiddleware
